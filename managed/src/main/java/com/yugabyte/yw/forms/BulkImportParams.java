6132572b8d927c5eca1377f33fd8e17dfeb84814
Jaydeep Ahir <jaydeepkumar.a@hashedin.com>
2021-06-25 14:47:38 +0530

    Added required

--- managed/src/main/java/com/yugabyte/yw/forms/BulkImportParams.java
+++ managed/src/main/java/com/yugabyte/yw/forms/BulkImportParams.java
@@ -12,7 +12,7 @@ public class BulkImportParams extends TableManagerParams {
 
   // S3 bucket containing data to be bulk imported (should be of format s3://<path>/)
   @Constraints.Required
-  @ApiModelProperty(value = "S3 bucket URL")
+  @ApiModelProperty(value = "S3 bucket URL", required = true)
   public String s3Bucket;
 
   // Number of task nodes for the EMR job. Optional.
